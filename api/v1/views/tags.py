61965372f11740b59140dee35c2d14a039473c50
duncmv <dtindiwensi@gmail.com>
2024-04-22 17:26:37 +0300

    initial routes

--- api/v1/views/tags.py
+++ api/v1/views/tags.py
@@ -16,3 +16,14 @@ def tag(user_id, poll_id=None):
             abort(404)
         tags = [tag.poll_id for tag in tags if tag.user_id == user_id]
         return jsonify(tags)
+
+    if request.method == 'POST':
+        new = Tag(user_id=user_id, poll_id=poll_id)
+        new.save()
+        return jsonify(new.to_dict()), 201
+
+    if request.method == 'DELETE':
+        for tag in Storage.all(Tag):
+            if tag.user_id == user_id and tag.poll_id == poll_id:
+                tag.delete()
+                
