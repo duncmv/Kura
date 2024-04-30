7d1a1a11a12b83f87a294e4797587d67e6fea34b
duncmv <dtindiwensi@gmail.com>
2024-04-30 18:39:05 +0300

    hot fix

--- api/v1/views/institutions.py
+++ api/v1/views/institutions.py
@@ -55,7 +55,7 @@ def institution(institution_id=None):
         params = request.get_json(silent=True)
         if not params:
             return make_response("Not a JSON\n", 400)
-        for k in ("id", "email", "created_at", "updated_at"):
+        for k in ("__class__", "id", "email", "created_at", "updated_at"):
             params.pop(k, None)
         for k, v in params.items():
             setattr(institution, k, v)
