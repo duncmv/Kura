b200952f182d5f176f56158a1bd42c0652eed5f2
duncmv <dtindiwensi@gmail.com>
2024-05-05 23:05:15 +0300

    update profile fix

--- api/v1/ext/upload.py
+++ api/v1/ext/upload.py
@@ -6,15 +6,15 @@ def upload_to_s3(file_name, object_name=None):
     # If S3 object_name was not specified, use file_name
     if object_name is None:
         object_name = file_name
-    with open(file_name, 'rb') as file:
-        img_test = file.read()
-    new_file = f"/tmp/{object_name}"
-    with open(new_file, "wb") as file:
-        file.write(img_test)
+    #with open(file_name, 'rb') as file:
+    #    img_test = file.read()
+    #new_file = f"/tmp/{object_name}"
+    #with open(new_file, "wb") as file:
+    #    file.write(img_test)
     # Upload the file
     s3_client = boto3.client('s3')
-    response = s3_client.upload_file(new_file, "kuradev2", object_name)
-    os.remove(new_file)
+    response = s3_client.upload_file(file_name, "kuradev2", object_name)
+    os.remove(file_name)
 
     # Get the object URL
     object_url = f"https://kuradev2.s3.amazonaws.com/{object_name}"
